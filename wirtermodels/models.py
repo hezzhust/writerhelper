bb9016cf55ca8c32e66b733a0ef99406e3d609b9
hezz <271743034@qq.com>
2018-07-03 19:37:17 +0800

    提交数据库设计

--- wirtermodels/models.py
+++ wirtermodels/models.py
@@ -11,115 +11,99 @@ class BaseModel(models.Model):
     modify_time = models.DateTimeField(blank=True,null=True)
     status = models.IntegerField(default=0)  # 0-隐藏，1-发布，-1删除
     ops_user_id = models.CharField(max_length= 50, blank=True, null=True) #操作员工id
-
-    class Meta:
-        abstract = True
-
-
-class BaseDictModel(BaseModel):
-    name = models.CharField(max_length=100)  # 名称
-
-    class Meta:
-        abstract = True
-
-
-class BaseDataModel(BaseModel):
-    name = models.CharField(max_length=100)  # 名称
+    name = models.CharField(max_length=400, db_index=True)  # 名称
 
     class Meta:
         abstract = True
 
 
 # --------------------字典表----------------------
-# 姓
-class DictFirstName(BaseDictModel):
-    name = models.CharField(max_length=50, db_index=True)  # 名称
+# 类型字段表，定义字段名称
+class DictTypeKey(BaseModel):
+    class Meta:
+        db_table = "dict_type_key"
+
+# 类型字段内容表， 定义字段KEY 对应的选项值
+class DictTypeValue(BaseModel):
+    typeKey = models.ForeignKey(DictTypeKey, models.DO_NOTHING)
+    class Meta:
+        db_table = "dict_type_value"
+
+
+
+# 名称字典表 该表存储原始字典数据，主要用于随机生成名字
+# type:{0,1,2}  对应{姓，名，字}
+
+class DictName(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 来源
+    language = models.IntegerField(default=0);# 0-中文，1-英文
+    type = models.IntegerField(default=0); # 0-姓，1-名，2-字
 
     class Meta:
-        db_table = "dict_first_name"
+        db_table = "dict_name"
 
 
-# 名
-class DictSecondName(BaseDictModel):
-    name = models.CharField(max_length=50, db_index=True)  # 名称
-    describe = models.CharField(max_length=2000, null=True)  # 来源
+class DictPoem(BaseModel):
 
     class Meta:
-        db_table = "dict_second_name"
+        db_table = "dict_poem"
 
 
-# 字
-class DictThridName(BaseDictModel):
-    name = models.CharField(max_length=50, db_index=True)  # 名称
-    describe = models.CharField(max_length=2000, null=True)  # 来源
-
+# 区域字典表
+# type:{0,1,2,3,4,5} 对应无，国家，省市，市县，乡镇，村
+class DictArea(BaseModel):
+    describe = models.CharField(max_length=2000, null=True)  # 描述
+    type = models.IntegerField(default=0); # 0-无，1-国家，2-省市，3-市县，4-乡镇，5-村
     class Meta:
-        db_table = "dict_third_name"
+        db_table = "dict_area"
 
 
 # 类型表
-class DictType(BaseDictModel):
+# tag 标签
+class DictType(BaseModel):
+    describe = models.CharField(max_length=1000, null=True)  # 描述
+    tag = models.IntegerField(default=0); # 0-无，1-国家，2-省市，3-市县，4-乡镇，5-村
     class Meta:
         db_table = "dict_type"
 
 
-# 标签
-class DictTag(BaseDictModel):
-    class Meta:
-        db_table = "dict_tag"
 
-
-# 头衔
-class DictTitle(BaseDictModel):
-    class Meta:
-        db_table = "dict_title"
-
-
-# 国家
-class DictCountry(BaseDictModel):
-    class Meta:
-        db_table = "dict_country"
 
 
 # --------------------常规业务表----------------------
 
-# 企业
-class Company(BaseDataModel):
-    address = models.CharField(max_length=100, null=True)  # 地址
 
-    class Meta:
-        db_table = "data_company"
 
 
 # 作家 数据表
-class Author(BaseDataModel):
+class Author(BaseModel):
     summary = models.CharField(max_length=500, blank=True, null=True)  # 简介
-    country = models.ForeignKey(DictCountry, models.DO_NOTHING, blank=True, null=True)  # 国家
-    titles = models.ManyToManyField(DictTitle, db_table='mid_author_title')  # 头衔
+    # country = models.ForeignKey(DictCountry, models.DO_NOTHING, blank=True, null=True)  # 国家
+    country = models.CharField(max_length=100, blank=True, null=True) # 国家
+    titles = models.CharField(max_length=200, blank=True, null=True)    # 头衔
 
     class Meta:
         db_table = "data_author"
 
 
 # 书籍 数据表
-class Book(BaseDataModel):
+class Book(BaseModel):
     authors = models.ManyToManyField(Author, db_table='mid_book_author')  # 作者
     tags = models.ManyToManyField(DictTag, db_table='mid_book_tag')  # 标签
     type = models.ForeignKey(DictType, models.DO_NOTHING, blank=True, null=True)  # 类型
-    country = models.ForeignKey(DictCountry, models.DO_NOTHING, blank=True, null=True)  # 国家
+    country = models.CharField(max_length=100, blank=True, null=True) # 国家
     summary = models.CharField(max_length=1000, null=True)  # 摘要
     content = models.TextField(null=True)  # 文本内容 如果没有章节 则直接存储文本内容
     chapter_count = models.IntegerField(default=0)  # 章节数
     source = models.CharField(max_length=100, null=True)  # 来源
-    public_company = models.ForeignKey(Company, models.DO_NOTHING, blank=True, null=True)  # 出版社
+    public_company = models.CharField(max_length=100, null=True)  # 出版社
 
     class Meta:
         db_table = "data_book"
 
 
 # 章节 数据表
-class Chapter(BaseDataModel):
+class Chapter(BaseModel):
     book = models.ForeignKey(Book, models.DO_NOTHING)
     content = models.TextField(null=True)  # 文本内容 如果没有章节 则直接存储文本内容
 
@@ -141,7 +125,7 @@ class Sentence(BaseModel):
 
 # ----------------- 设定表 ------------------------
 # 人物设定表
-class SettingPerson(BaseDataModel):
+class SettingPerson(BaseModel):
     appearance = models.CharField(max_length=1000, null=True)  # 外貌描述
     character = models.CharField(max_length=1000, null=True)  # 性格描述
     describe = models.CharField(max_length=2000, null=True)  # 其他描述
@@ -156,7 +140,7 @@ class SettingPerson(BaseDataModel):
 
 
 # 道具设定表
-class SettingProp(BaseDataModel):
+class SettingProp(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 描述
     level = models.CharField(max_length=100, null=True)  # 品质等级
     book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
@@ -165,8 +149,8 @@ class SettingProp(BaseDataModel):
         db_table = "setting_prop"
 
 
-# 法术设定表
-class SettingMagic(BaseDataModel):
+# 技能设定表
+class SettingMagic(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 描述
     level = models.CharField(max_length=100, null=True)  # 品质等级
     book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
@@ -176,7 +160,7 @@ class SettingMagic(BaseDataModel):
 
 
 # 世界设定表
-class SettingWorld(BaseDataModel):
+class SettingWorld(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 描述
     level = models.CharField(max_length=100, null=True)  # 品质等级
     book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
@@ -186,7 +170,7 @@ class SettingWorld(BaseDataModel):
 
 
 # 势力设定表
-class SettingPower(BaseDataModel):
+class SettingPower(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 描述
     level = models.CharField(max_length=100, null=True)  # 品质等级
     book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
@@ -196,7 +180,7 @@ class SettingPower(BaseDataModel):
 
 
 # 其他设定表
-class SettingOther(BaseDataModel):
+class SettingOther(BaseModel):
     describe = models.CharField(max_length=2000, null=True)  # 描述
     level = models.CharField(max_length=100, null=True)  # 品质等级
     book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
@@ -206,3 +190,12 @@ class SettingOther(BaseDataModel):
 
     class Meta:
         db_table = "setting_other"
+
+#地图设定表
+class SettingMap(BaseModel):
+    describe = models.CharField(max_length=2000, null=True)  # 描述
+    level = models.CharField(max_length=100, null=True)  # 品质等级
+    book = models.ForeignKey(Book, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        db_table = "setting_map"
